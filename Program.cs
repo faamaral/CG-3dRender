b6de924dedee8aaaabf6b9e683f7fe9e1f88c11b
faamaral <fabianoamaral445@gmail.com>
2023-02-28 21:09:24 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using OpenTK.Mathematics;
+using OpenTK.Windowing.Desktop;
+
+namespace _3dRender
+{
+    public static class Program
+    {
+        private static void Main()
+        {
+            var _cfg = new NativeWindowSettings()
+            {
+                Size = new Vector2i(800, 600),
+                Title = "Olá Triângulo",
+            };
+
+            using (var window = new Window(GameWindowSettings.Default, _cfg)) { window.Run(); }
+        }
+    }
+}
---
+++ Window.cs
@@ -0,0 +1,16 @@
+﻿using OpenTK.Windowing.Desktop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3dRender
+{
+    internal class Window : GameWindow
+    {
+        public Window(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings) : base(gameWindowSettings, nativeWindowSettings)
+        {
+        }
+    }
+}
